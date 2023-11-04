fbbb69c6dd29f1a698bdd5022f374e1ca7c7c5a4
ANDREY LEBEDEV <lebedev88@gmail.com>
2023-11-04 18:33:48 +0300

    HW2 completed

---
+++ HW2/item_fabric.py
@@ -0,0 +1,103 @@
+import random
+from abc import ABC, abstractmethod
+
+
+class IGameItem(ABC):
+    @abstractmethod
+    def open(self):
+        pass
+
+
+class IGameFabric(ABC):
+    @abstractmethod
+    def create_item(self):
+        pass
+
+
+class Gold(IGameItem):
+    def open(self):
+        print("This is gold!")
+
+
+class Gem(IGameItem):
+    def open(self):
+        print("This is gem!")
+
+
+class Silver(IGameItem):
+    def open(self):
+        print("This is silver!")
+
+
+class Platinum(IGameItem):
+    def open(self):
+        print("This is platinum!")
+
+
+class Diamond(IGameItem):
+    def open(self):
+        print("This is diamond!")
+
+
+class Emerald(IGameItem):
+    def open(self):
+        print("This is emerald!")
+
+
+class Titanium(IGameItem):
+    def open(self):
+        print("This is titanium!")
+
+
+class GoldGeneator(IGameFabric):
+    def create_item(self):
+        return Gold()
+
+
+class GemGeneator(IGameFabric):
+    def create_item(self):
+        return Gem()
+
+
+class SilverGeneator(IGameFabric):
+    def create_item(self):
+        return Silver()
+
+
+class PlatinumGeneator(IGameFabric):
+    def create_item(self):
+        return Platinum()
+
+
+class DiamondGeneator(IGameFabric):
+    def create_item(self):
+        return Diamond()
+
+
+class EmeraldGeneator(IGameFabric):
+    def create_item(self):
+        return Emerald()
+
+
+class TitaniumGeneator(IGameFabric):
+    def create_item(self):
+        return Titanium()
+
+
+if __name__ == "__main__":
+    generators = [SilverGeneator()] * 10 + [PlatinumGeneator()] * 10 + [DiamondGeneator()] * 10 + [
+        TitaniumGeneator()] * 10 + [EmeraldGeneator()] * 10 + [GoldGeneator()] * 3 + [GemGeneator()]
+
+    rewards = []
+
+    counter = {}
+
+    for _ in range(1_000_000):
+        rewards.append(item := random.choice(generators).create_item())
+        item_name = type(item).__name__
+        if item_name in counter.keys():
+            counter[item_name] += 1
+        else:
+            counter[item_name] = 1
+
+    print(counter)
